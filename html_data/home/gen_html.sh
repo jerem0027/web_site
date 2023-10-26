b667450b5c35e0e14ce013feb6b5d3a389de8d81
Jeremie Henrion <jeremie_henrion@carrefour.com>
2023-10-26 13:59:37 +0200

    Feat: change about us page

--- html_data/home/gen_html.sh
+++ html_data/home/gen_html.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 node ../../tools/create_file.js ./templates/index.html.handlebars ./none.yml ../../html/home/index.html
-node ../../tools/create_file.js ./templates/inscription.html.handlebars ./none.yml ../../html/home/inscription.html 
-node ../../tools/create_file.js ./templates/guest.html.handlebars ./none.yml ../../html/home/guest.html 
+node ../../tools/create_file.js ./templates/inscription.html.handlebars ./none.yml ../../html/home/inscription.html
+node ../../tools/create_file.js ./templates/guest.html.handlebars ./none.yml ../../html/home/guest.html
+node ../../tools/create_file.js ./templates/abous_us.html.handlebars ./none.yml ../../html/home/about_us.html
+
