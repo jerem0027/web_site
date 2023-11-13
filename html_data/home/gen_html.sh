6c530bfe36a3110e2fa31636671515790b769f0a
Jeremie Henrion <jeremie_henrion@carrefour.com>
2023-11-13 23:53:32 +0100

    Fix: update templating home/marvel-fan

--- html_data/home/gen_html.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-node ../../tools/create_file.js ./templates/index.html.handlebars ./home.yml ../../html/home/index.html
-node ../../tools/create_file.js ./templates/inscription.html.handlebars ./home.yml ../../html/home/inscription.html
-node ../../tools/create_file.js ./templates/guest.html.handlebars ./home.yml ../../html/home/guest.html
-node ../../tools/create_file.js ./templates/about_us.html.handlebars ./home.yml ../../html/home/about_us.html
-node ../../tools/create_file.js ./templates/account.html.handlebars ./home.yml ../../html/home/account.html
-node ../../tools/create_file.js ./templates/connection.html.handlebars ./home.yml ../../html/home/connection.html
