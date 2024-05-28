b1fbed988c1e46a3bdd9e1dceb5fb3e68b12af2d
Jeremie Henrion <jeremie_henrion@carrefour.com>
2024-05-28 02:58:20 +0200

    Fix: update Inscription page

--- html/home/js/inscription.js
+++ html/home/js/inscription.js
@@ -15,6 +15,29 @@ const init = function() {
         $('#' + $(this)[0].name).val(localStorage.getItem($(this)[0].name));
     });
     before_pseudo();
+
+    $('#birthdate').on('input', (event) => {
+        update_birthdate(event);
+    });
+}
+
+const update_birthdate = function(event) {
+    let data = $('#birthdate').val()
+    if((!(event.data >= 0 && event.data <=9)) || (event.inputType == "deleteContentBackward")) {
+        if(data[data.length - 1] == "/") {
+            $('#birthdate').val(data.slice(0, data.length - 1));
+            return;
+        }
+    }
+    else {
+        if(data.length >= 2 && data[2] != "/") {
+            $('#birthdate').val(data.slice(0, 2) + "/" + data.slice(2, data.length))
+        }
+        if(data.length >= 5 && data[5] != "/")
+            $('#birthdate').val(data.slice(0, 5) + "/" + data.slice(5, data.length))
+        if(data.length > 10)
+            $('#birthdate').val(data.slice(0, 10));
+    }
 }
 
 const send_inscription = async function() {
