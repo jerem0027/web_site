2c0c1f5d9acf6fa9bed858543c233cc21cc1a36e
Jeremie Henrion <jeremie_henrion@carrefour.com>
2024-05-31 16:13:42 +0200

    Fix: no log in prod

--- html/home/js/constantes.js
+++ html/home/js/constantes.js
@@ -8,11 +8,9 @@ const test = false;
 const copy_clipboard = function(element) {
     navigator.clipboard.writeText($("#" + element).html());
 
-    console.log($("#btn_copy_clipboard i").html("check"));
-
     $('#btn_copy_clipboard').addClass("change_color");
     setTimeout(() => {
         $('#btn_copy_clipboard').removeClass("change_color");
-        console.log($("#btn_copy_clipboard i").html("content_copy"));
+        $("#btn_copy_clipboard i").html("content_copy");
     }, 5000);
 }
